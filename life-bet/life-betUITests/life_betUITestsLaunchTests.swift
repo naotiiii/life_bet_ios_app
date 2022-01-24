26b7e7f826a7fa3e9954375370e30ecb357975e7
naoto kojima <>
2022-01-24 22:16:37 +0900

    xcode プロジェクト作成

---
+++ life-bet/life-betUITests/life_betUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  life_betUITestsLaunchTests.swift
+//  life-betUITests
+//
+//  Created by naoto kojima on 2022/01/24.
+//
+
+import XCTest
+
+class life_betUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
